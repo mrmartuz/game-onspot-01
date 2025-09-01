243ab66e61207966da310ee445f9911479ae9fb0
mrmartuz <mattia92martinelli@gmail.com>
2025-09-01 10:48:21 +0200

    fix(death-dialog): fix missing button

--- interactions/deathDialog.js
+++ interactions/deathDialog.js
@@ -4,17 +4,17 @@ import { getShowChoiceDialog } from '../interactions.js';
 export async function showDeathDialog(death){
     if(death === 'health'){
         await getShowChoiceDialog('You died fighting! ☠️', [
-            { label: '🔄 Restart Game', value: 'restart' }
+            { type: 'button', label: '🔄 Restart Game', value: 'restart' }
         ]);
         location.reload();
     } else if(death === 'gold'){
         await getShowChoiceDialog('You paid your debt with your life! ☠️', [
-            { label: '🔄 Restart Game', value: 'restart' }
+            { type: 'button', label: '🔄 Restart Game', value: 'restart' }
         ]);
         location.reload();
     } else if(death === 'hunting'){
         await getShowChoiceDialog('The hunter became the hunted! ☠️ \n You died!', [
-            { label: '🔄 Restart Game', value: 'restart' }
+            { type: 'button', label: '🔄 Restart Game', value: 'restart' }
         ]);
         location.reload();
     }
